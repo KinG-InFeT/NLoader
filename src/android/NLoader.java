87b8f2868e9656f880a608bec6ff564dcbbf04d1
KinG-InFeT <king.infet@gmail.com>
2014-02-07 13:39:15 +0100

    Format code

--- src/android/NLoader.java
+++ src/android/NLoader.java
@@ -11,7 +11,6 @@
 
 package org.apache.cordova.nloader;
 
-
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -22,8 +21,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.luongovincenzo.tools.R;
-
+import com.phonegap.linkpass.R;
 
 //import android.provider.Settings;
 
@@ -32,111 +30,118 @@ public class NLoader extends CordovaPlugin {
 	private ProgressDialog waitingDialog = null;
 
 	/**
-     * Constructor.
-     */
-    public NLoader() {
-    }
+	 * Constructor.
+	 */
+	public NLoader() {
+	}
 
 	@Override
-    public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-		 
-        cordova.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-            	if (action.equals("startloader")) {
-        			
-        			JSONObject jo;
-					
-        			try {
-        				jo = args.getJSONObject(0);
-						StartLoader(jo.getString("title"), jo.getString("text"), jo.getInt("cancelable"), callbackContext);
+	public boolean execute(final String action, final JSONArray args,
+			final CallbackContext callbackContext) throws JSONException {
+
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				if (action.equals("startloader")) {
+
+					JSONObject jo;
+
+					try {
+						jo = args.getJSONObject(0);
+						StartLoader(jo.getString("title"),
+								jo.getString("text"), jo.getInt("cancelable"),
+								callbackContext);
 					} catch (JSONException e) {
 						e.printStackTrace();
-					} 
-        			
-                } else if(action.equals("stoploader")) {
-        		
-        			StopLoader(callbackContext);
-        			
-        		} 
-        		
-        		callbackContext.success();
-            }
-        });
-        return true;
-    }
-	
-	public void StartLoader(String title, String text, Integer cancelable, CallbackContext callbackContext) {
-	
-		//Default value
-		if(text == "") {
+					}
+
+				} else if (action.equals("stoploader")) {
+
+					StopLoader(callbackContext);
+
+				}
+
+				callbackContext.success();
+			}
+		});
+		return true;
+	}
+
+	public void StartLoader(String title, String text, Integer cancelable,
+			CallbackContext callbackContext) {
+
+		// Default value
+		if (text == "") {
 			text = "Please wait";
 		}
-		
+
 		this.showWaitingDialog(title, text, cancelable);
 		callbackContext.success();
 
 	}
-	
+
 	public void StopLoader(CallbackContext callbackContext) {
 		this.hideWaitingDialog();
 		callbackContext.success();
 	}
-	
-	//Avvio il loader
+
+	// Avvio il loader
 	public void showWaitingDialog(String title, String text, Integer cancelable) {
-		//OLD
-		//waitingDialog = ProgressDialog.show(this.cordova.getActivity(), "", text);
-		
-		//NEW
-		//Avvio l'istanza
+		// OLD
+		// waitingDialog = ProgressDialog.show(this.cordova.getActivity(), "",
+		// text);
+
+		// NEW
+		// Avvio l'istanza
 		waitingDialog = new ProgressDialog(this.cordova.getActivity());
-		
-		//setto se è possibile cancellarla al tocco dello schermo
-		if(cancelable == 1) {
+
+		// setto se è possibile cancellarla al tocco dello schermo
+		if (cancelable == 1) {
 			waitingDialog.setCancelable(true);
-		}else{
+		} else {
 			waitingDialog.setCancelable(false);
 		}
-		
-		//Setto il testo da apparire
+
+		// Setto il testo da apparire
 		waitingDialog.setMessage(text);
-		
-		//Setto il titolo
-		if(title != "") {
+
+		// Setto il titolo
+		if (title != "") {
 			waitingDialog.setTitle(title);
 		}
-		
+
 		waitingDialog.setIndeterminate(false);
-		
-		//Setto di che tipologia deve essere la progressbar
+
+		// Setto di che tipologia deve essere la progressbar
 		waitingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-		
-		waitingDialog.getWindow().setLayout(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		
-		//waitingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
-		
+
+		waitingDialog.getWindow().setLayout(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+
+		// waitingDialog.getWindow().setBackgroundDrawable(new
+		// ColorDrawable(0));
+
 		waitingDialog.setIcon(R.drawable.icon);
-		
+
 		// FOR -> STYLE_HORIZONTAL
-		//waitingDialog.setProgress(0);
-		//waitingDialog.setMax(100);
-		
-		//Appare
+		// waitingDialog.setProgress(0);
+		// waitingDialog.setMax(100);
+
+		// Appare
 		waitingDialog.show();
-			
+
 	}
-	
+
 	public void hideWaitingDialog() {
-	
-		if (waitingDialog != null) { //Aperto? chiudilo
-		
+
+		if (waitingDialog != null) { // Aperto? chiudilo
+
 			waitingDialog.dismiss();
 			waitingDialog = null;
-			
+
 		} else {// altrimenti gestisco l'exeption e non eseguo niente
-		
-			//LOG.d("WaitingDialog", "Nothing to dismiss");
+
+			// LOG.d("WaitingDialog", "Nothing to dismiss");
 		}
 	}
-	
+
 }
