9d780dec872ba29574c605af3a4b35376c838abd
KinG-InFeT <king.infet@gmail.com>
2014-02-10 13:13:26 +0100

    Revert call super function

--- src/ios/NLoader.m
+++ src/ios/NLoader.m
@@ -70,7 +70,7 @@
     //[self.NLoader addSubview:indicator];
 	
 	
-	//[super showNLoaderWithText];
+	[super showNLoaderWithText];
  
      loadingView = [[UIView alloc] initWithFrame:CGRectMake(75, 155, 170, 170)];
      loadingView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
