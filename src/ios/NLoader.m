596f297b06b2d1e37cd2e9ad3478f04d4fca20d3
KinG-InFeT <king.infet@gmail.com>
2014-02-12 09:36:13 +0100

    Adaptation and iOS compatibility 7. Many of BUG fixes

--- src/ios/NLoader.m
+++ src/ios/NLoader.m
@@ -9,44 +9,66 @@
 - (void)loadingStart:(CDVInvokedUrlCommand*) command
 
 {
+   NSLog(@"dentro loadingStart");
     if([command.arguments count] > 0){
         NSDictionary* optionsdic = [command.arguments objectAtIndex:0];
     
         if (self.loadingView != nil) {
             return;
         }
+        
+        NSLog(@"dentro if arguments");
 	
         CGFloat strokeOpacity, backgroundOpacity;
         CGFloat boxLength = [NLoaderView defaultBoxLength];
         BOOL fullScreen = YES;
         BOOL bounceAnimation = NO;
+        
+         NSLog(@"AZZ 1");
+        
         NSString* colorCSSString;
         NSString* labelText;
+
+        NSLog(@"AZZ 1A");
+
+        strokeOpacity = 0.00;//[[optionsdic objectForKey: @"strokeOpacity"] floatValue];
+        backgroundOpacity = 0.00;//[[optionsdic objectForKey: @"backgroundOpacity"] floatValue];
         
-        strokeOpacity = [[optionsdic objectForKey:@"strokeOpacity"] floatValue];
-        backgroundOpacity = [[optionsdic objectForKey:@"backgroundOpacity"] floatValue];
+        NSLog(@"AZZ 2");
         
-        id fullScreenValue = [optionsdic objectForKey:@"fullScreen"];
+        id fullScreenValue = NULL;//[optionsdic objectForKey: @"fullScreen"];
+        
+        NSLog(@"AZZ 2A");
+        fullScreen = [fullScreenValue boolValue];
+        boxLength = fmax(boxLength, 1.00/*[[optionsdic objectForKey: @"boxLength"] floatValue]*/);
+        /*
         if (fullScreenValue != nil)
         {
             fullScreen = [fullScreenValue boolValue];
             if (!fullScreen) { // here we take into account rectSquareLength, if any
-                boxLength = fmax(boxLength, [[optionsdic objectForKey:@"boxLength"] floatValue]);
+                boxLength = fmax(boxLength, [[optionsdic objectForKey: @"boxLength"] floatValue]);
             }
         }
+        */
+        NSLog(@"AZZ 3");
         
-        id bounceAnimationValue = [optionsdic objectForKey:@"bounceAnimation"];
+        id bounceAnimationValue = NULL;//[optionsdic objectForKey:@"bounceAnimation"];
+        bounceAnimation = [bounceAnimationValue boolValue];
+        /*
         if (bounceAnimationValue != nil)
         {
             bounceAnimation = [bounceAnimationValue boolValue];
         }
-        
-        colorCSSString = [optionsdic objectForKey:@"strokeColor"];
-        labelText = [optionsdic objectForKey:@"labelText"];
-        
+        */
+        NSLog(@"AZZ 3A");
+        colorCSSString = @"";//@"#333";//[optionsdic objectForKey:@"strokeColor"];
+        labelText = @"Loader test azz";//[optionsdic objectForKey:@"labelText"];
+
         if (!labelText) {
             labelText = [NLoaderView defaultLabelText];
         }
+        
+        NSLog(@"AZZ 4");
         
         UIColor* strokeColor = [NLoaderView defaultStrokeColor];
         
@@ -57,6 +79,8 @@
         if (backgroundOpacity <= 0) {
             backgroundOpacity = [NLoaderView defaultBackgroundOpacity];
         }
+        
+        NSLog(@"AZZ 5");
         
         if (colorCSSString) {
             UIColor* tmp = [UIColor colorWithName:colorCSSString];
@@ -70,22 +94,36 @@
             }
         }
         
+        NSLog(@"AZZ 6");
+        
         self.loadingView = [NLoaderView loadingViewInView:[super viewController].view strokeOpacity:strokeOpacity
                                            backgroundOpacity:backgroundOpacity
                                                  strokeColor:strokeColor fullScreen:fullScreen labelText:labelText
                                              bounceAnimation:bounceAnimation boxLength:boxLength];
-        
+
+        NSLog(@"AZZ 7");
+
         NSRange minMaxDuration = NSMakeRange(2, 3600);// 1 hour max? :)
+
         NSString* durationKey = @"duration";
+        //NSInteger durationKey = 10;
+
+        NSLog(@"AZZ 8");
+
         // the view will be shown for a minimum of this value if durationKey is not set
-        self.loadingView.minDuration = [optionsdic integerValueForKey:@"minDuration" defaultValue:minMaxDuration.location withRange:minMaxDuration];
-        
+        //self.loadingView.minDuration = [optionsdic integerValueForKey:@"minDuration" defaultValue:minMaxDuration.location withRange:minMaxDuration];
+
+        NSLog(@"AZZ 8");
+
         // if there's a duration set, we set a timer to close the view
-        if ([optionsdic valueForKey:durationKey]) {
-            NSTimeInterval duration = [optionsdic integerValueForKey:durationKey defaultValue:minMaxDuration.location withRange:minMaxDuration];
-            [self performSelector:@selector(loadingStop:) withObject:nil afterDelay:duration];
-        }
+        //if ([optionsdic valueForKey:durationKey]) {
+            //NSTimeInterval duration = [optionsdic integerValueForKey:durationKey defaultValue:minMaxDuration.location withRange:minMaxDuration];
+            //[self performSelector:@selector(loadingStop:) withObject:nil afterDelay:duration];
+        //}
+        NSLog(@"AZZ 9");
     }
+    
+    NSLog(@"AZZ 10");
 }
 
 - (void)loadingStop: (CDVInvokedUrlCommand*) command
